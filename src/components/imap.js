0c0b033029eed5b3d450db02204672ed87238b6a
Serhii Kalachnikov <alikil@ukr.net>
2019-08-15 08:00:15 +0300

    imap important

--- src/components/imap.js
+++ src/components/imap.js
@@ -9,6 +9,9 @@ exports.Gmail = async(email,passToEmail,parseFirstPart,parseSecondPart,status) =
         host: 'imap.gmail.com',
         port: 993,
         tls: true,
+        tlsOptions: {
+          rejectUnauthorized: false
+        },
         authTimeout: 3000
       }
     };
